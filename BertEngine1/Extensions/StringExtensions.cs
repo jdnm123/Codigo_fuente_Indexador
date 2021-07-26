49c689adf2ebc13f7e0efc49f94fec01ac38dfc8
jdnm123 <87243395+jdnm123@users.noreply.github.com>
2021-07-25 21:17:21 -0500

    Add files via upload

---
+++ BertEngine1/Extensions/StringExtensions.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace Microsoft.ML.Models.BERT.Extensions
+{
+    public static class StringExtensions
+    {
+        public static string CleanNames(string input)
+        {
+            Regex r = new Regex("(?:[^a-z ]|(?<=['\"])s)", RegexOptions.IgnoreCase | RegexOptions.CultureInvariant | RegexOptions.Compiled);
+            string v = r.Replace(input, " ");
+            v = v.Replace(" ", "");
+            while (v.Contains("  "))
+            {
+                v = v.Replace("  ", " ");
+            }
+            return v.ToLower().TrimEnd(' ').TrimStart(' ');
+        }
+
+        public static IEnumerable<string> SplitAndKeep(this string s, params char[] delimiters)
+        {
+            int start = 0, index;
+
+            while ((index = s.IndexOfAny(delimiters, start)) != -1)
+            {
+                if (index - start > 0)
+                    yield return s.Substring(start, index - start);
+
+                yield return s.Substring(index, 1);
+
+                start = index + 1;
+            }
+
+            if (start < s.Length)
+            {
+                yield return s.Substring(start);
+            }
+        }
+    }
+}
---
+++ BertEngine1/Input/BertFeature.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.ML.Data;
+using System.Collections.Generic;
+
+namespace Microsoft.ML.Models.BERT.Input
+{
+    internal class BertFeature
+    {
+        //[VectorType(1)]
+        //[ColumnName("unique_ids:0")]
+        //public long[] UniqueIds { get; set; }
+
+        [VectorType(1, 512)]
+        [ColumnName("attention_mask")]
+        public long[] InputMask { get; set; }
+
+        [VectorType(1, 512)]
+        [ColumnName("input.2")]
+        public long[] token_type_ids { get; set; }
+
+        [VectorType(1, 512)]
+        [ColumnName("input.1")]
+        public long[] InputIds { get; set; }
+    }
+}
